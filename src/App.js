b91939c271ed7a6084312b529085f6ab1a3a3e8c
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-02-23 06:48:21 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Nav from './Components/Nav'
+import routes from './routes';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'></div>
+    <div className='App'>
+      <Nav/>
+      {routes}
+    </div>
   )
 };
 
