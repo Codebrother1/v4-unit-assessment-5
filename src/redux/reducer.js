1682f3de0e61bac422201066079f8b8b045f8a73
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-02-23 11:34:18 -0600

    reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,32 @@
 const initialState = {
-  
+  username: {},
+  profile_pic: {}
+}
+
+export function updateUser(){
+  return{
+    type: UPDATE_USER,
+    payload: profile_pic
+  }
+}
+export function logout(){
+  return {
+      type: LOGOUT,
+      payload: initialState
+  }
+}
+
+
+
+
+
+
+export default function reducer(state = initialState, action){
+switch(action.type){
+  case UPDATE_USER:
+    return{...state, user: action.payload};
+  case LOGOUT:
+      return initialState;
+}
+
 }
